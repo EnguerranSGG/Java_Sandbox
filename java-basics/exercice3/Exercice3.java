42183a82fa57e7927087772facd705b15fe229f8
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2024-10-27 11:13:04 +0100

    feat(java-basics): terminate third exercice

---
+++ java-basics/exercice3/Exercice3.java
@@ -0,0 +1,11 @@
+public class Exercice3 {
+
+    public static void main(String[] args) {
+
+        double circleRadius = 3;
+        double piValue = Math.PI;
+        
+        System.out.println(2 * piValue * circleRadius);
+    }
+
+}

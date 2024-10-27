d48e46e8e4c6ba6566ace885ee7c259db829292c
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2024-10-27 22:12:58 +0100

    feat(java-basics): terminate 6th exercice

---
+++ java-basics/exercice6/Exercice6.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+// À partir d’une valeur saisie, indiquer si le nombre saisi est pair ou impair
+
+public class Exercice6 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Entrez un nombre :");
+        int userNumber = scanner.nextInt();
+
+        if ((userNumber % 2) == 0) {
+            System.out.println(userNumber + " est un nombre pair !");
+        } else {
+            System.out.println(userNumber + " est un nombre impair !");
+        }
+
+        scanner.close();
+    }
+}

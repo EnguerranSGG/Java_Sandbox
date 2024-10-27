79f457e1d64d2d5a98a61e3e9e3f5012e7268892
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2024-10-27 10:16:48 +0100

    feat(java-basics): terminate 2th exercice

---
+++ java-basics/Exercice2.java
@@ -0,0 +1,8 @@
+public class Exercice2 {
+
+    public static void main(String[] args) {
+        String helloJavaString = "Hello Java !";
+
+        System.out.println(helloJavaString);
+    }
+    }
